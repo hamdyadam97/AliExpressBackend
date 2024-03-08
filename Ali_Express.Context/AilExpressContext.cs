fd27356bd1754fb7cb2a6b1413c03a5d5383d79e
hamdyadam97 <hamdyadam97@gmail.com>
2024-03-08 04:17:51 +0200

    models product category image and his realation between them

---
+++ Ali_Express.Context/AilExpressContext.cs
@@ -0,0 +1,24 @@
+﻿using Ali_Express.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ali_Express.Context
+{
+    public class AilExpressContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Images> Images { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public AilExpressContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProductCategory>()
+                .HasKey(pc => new { pc.ProductId, pc.CategoryId }); 
+        }
+    }
+}
